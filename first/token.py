108d2195c8b00f785f5d2f53198adfe8f88eb775
prithiga-pro <prithigaselva07@gmail.com>
2025-12-26 06:42:07 +0000

    new code

--- first/token.py
+++ first/token.py
@@ -1,17 +1,47 @@
-age=input("enter your age to take token")
-photo=input("you want to take photo  ?")
-if age<12:
-    print("token price is 150")
-        if photo=="yes"
-            print("you need to pay extra 50 rs")
-elif age>12 and age<18:
-    print("token price is 250")            
-            if photo=="yes"
-                print("you need to pay extra 50 rs")
-else:
-    print("token price is 500")
-    if photo=="yes"
-        print("you need to pay extra 50 rs")
+pizza=input("pizza size,small or medium or large   ")
+bill=0
+pepperoni=input("do you need pepperoni   ")
+cheese=input("do you want extra cheese  ")
+if pizza=="small":
+    bill+=150
+    print("for small pizza its 150")
+    
+    if pepperoni=="yes":
+        bill=bill+20
+        print("for pepperoni need to pay extra 20")
+        
+    if cheese=="yes":
+        bill=bill+20
+        print("for extra cheese need to pay 20")
+        print(bill)
+elif pizza=="medium":
+    
+    bill+=250
+    print("for medium pizza its 250")
+    
+    if pepperoni=="yes":
+        bill=bill+30
+        print("for pepperoni need to pay extra 30")
+        
+    if cheese=="yes":
+        bill=bill+20
+        print("for extra chees need to pay 20")
+        print(bill)
+
+elif pizza=="large":
+    bill+=350
+    print("for large pizza its 350")
+    
+    if pepperoni=="yes":
+        bill=bill+30
+        print("for pepperoni need to pay extra 30")
+        
+    if cheese=="yes":
+        bill=bill+20
+        print("for extra chees need to pay 20")
+        print(bill)
+    
+    
 
 
         
